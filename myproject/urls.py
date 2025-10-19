ca2618d1e19d8e893f7daf68799bf2e6e148f71e
Curby-lee Williams <curbris@gmail.com>
2025-10-19 14:21:47 +0200

    Update urls.py — added AJAX routes

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from app_profiles import views
@@ -25,9 +9,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', views.profiles_list, name='profiles_list'),
     path('signup/', views.signup, name='signup'),
-    path('edit_profile/', views.edit_profile, name='edit_profile')
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+
+    # New: AJAX login/logout
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
 
-# 👇 This is what serves MEDIA files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
