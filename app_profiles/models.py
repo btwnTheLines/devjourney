8afbe32f12d015d8abbf724d4c092fcc68944866
Curby-lee Williams <curbris@gmail.com>
2025-10-19 18:29:02 +0200

    Update models.py - Cloudinary avatar update

--- app_profiles/models.py
+++ app_profiles/models.py
@@ -1,11 +1,13 @@
-# Create your models here.
+from cloudinary_storage.storage import MediaCloudinaryStorage
 from django.contrib.auth.models import User
 from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     feedback = models.TextField(max_length=250, blank=False, default="Please add feedback")
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-
+    # avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', storage=MediaCloudinaryStorage())
+    
     def __str__(self):
+
         return self.user.username
