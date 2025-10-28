e182a3e1b6f0c32d42961cc6604a34b5f7211e9e
Curby-lee Williams <curbris@gmail.com>
2025-10-28 09:52:44 +0200

    Update urls.py - delete + logout

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,14 +7,17 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='home'),
     path('admin/', admin.site.urls),
-    path('profile/', views.profiles_list, name='profiles_list'),
+    path('profiles-list/', views.profiles_list, name='profiles_list'),
     path('signup/', views.signup, name='signup'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
 
     # New: AJAX login/logout
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    path("account/delete/", views.delete_account, name="account-delete"),
 ]
 
+# 👇 This is what serves MEDIA files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
